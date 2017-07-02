986660b92c4de78b2864de26d3761a3ff173072b
UserX <dubrovets.dmytro@gmail.com>
2017-07-02 20:50:24 +0300

    Task8,10: add

--- work/main.js
+++ work/main.js
@@ -39,8 +39,28 @@ function capitals() {
     var obj = {
         'Оттава': 'Канада',
         'Киев': 'Украина'
+    };
+
+    for (item in obj) {
+        console.log( item + " - это " + obj[item] );
     }
-
-
-
 }
+
+//capitals();
+
+//Task10================================
+//больше 3-х, но меньше 10.
+function numbersColumn() {
+    var numbers = [2, 5, 9, 15, 0, 4];
+    var i;
+
+    for ( i = 0; i < numbers.length; i++ ) {
+        if ( numbers[i] > 3 && numbers[i] < 10 ) {
+            console.log(numbers[i]);
+        }
+    }
+}
+
+numbersColumn();
+
+//Task11================================
