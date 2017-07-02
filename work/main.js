1fd100f9a8ef7fdf7bca2abeefbfe99c9d0a2998
UserX <dubrovets.dmytro@gmail.com>
2017-07-02 20:34:06 +0300

    Task5,6,7: add

--- work/main.js
+++ work/main.js
@@ -1,9 +1,46 @@
 //Task4================================
-
 function pow(x, n) {
     return Math.pow(x, n);
 }
 
-console.log( pow(2, 3) );
+//console.log( pow(2, 5) );
 
 //Task5================================
+function check(x, n) {
+    return x === n;
+}
+
+//console.log( check(3, 3) );
+
+//Task7================================
+function countWhile() {
+    var i = 0;
+
+    while (i < 50) {
+        i++;
+        console.log(i);
+    }
+}
+
+function countFor() {
+    var i;
+
+    for (i = 1; i <= 50; i++) {
+        console.log(i);
+    }
+}
+
+//countWhile();
+//countFor();
+
+//Task8================================
+
+function capitals() {
+    var obj = {
+        'Оттава': 'Канада',
+        'Киев': 'Украина'
+    }
+
+
+
+}
